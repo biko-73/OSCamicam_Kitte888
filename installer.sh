39e7f15019d0b51ea0b1e9d24ac466416b59ef9e
biko-73 <57875905+biko-73@users.noreply.github.com>
2023-07-01 21:20:07 +0300

    add deb

--- installer.sh
+++ installer.sh
@@ -13,6 +13,7 @@
 PACKAGE_DIR='OSCamicam_Kitte888/main'
 
 MY_IPK="enigma2-plugin-softcams-oscamicam_11718-V9-Kitte888-V16Rev_all.ipk"
+MY_DEB="enigma2-plugin-softcams-oscamicam_11718-V9-Kitte888-V17Rev_all.deb"
 
 ########################################################################################################################
 # Auto ... Do not change
