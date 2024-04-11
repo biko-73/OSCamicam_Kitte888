d59c7f1fb2765c1684ea437470a1434456fe0869
biko-73 <57875905+biko-73@users.noreply.github.com>
2024-04-12 01:58:42 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -12,8 +12,8 @@
 
 PACKAGE_DIR='OSCamicam_Kitte888/main'
 
-MY_IPK="enigma2-plugin-softcams-oscamicam_11748-ICAMnoEMU-Kitte888-V2Rev_all.ipk"
-MY_DEB="enigma2-plugin-softcams-oscamicam_11726-V9-V3-err-Kitte888-V9Rev_all.deb"
+MY_IPK="enigma2-plugin-softcams-oscamicam_11758-ICAMEMU-Kitte888-V6Rev_all.ipk"
+MY_DEB="enigma2-plugin-softcams-oscamicam_11758-ICAMEMU-Kitte888-V6Rev_all.deb"
 
 ########################################################################################################################
 # Auto ... Do not change
